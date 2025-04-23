87eb987cf3313520ed129364fc30e4ed848316f3
vyomPriyesh <vyompriyesh@gmail.com>
2025-04-23 17:02:19 +0530

    addd

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-// import { UserState } from './context/Userstate.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      {/* <UserState> */}
         <App />
-      {/* </UserState> */}
     </BrowserRouter>
   </StrictMode>,
 )
