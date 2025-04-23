6ced2eca6a87fa311aad8242ca96c9317098c477
vyomPriyesh <vyompriyesh@gmail.com>
2025-04-23 17:00:53 +0530

    addd

--- src/context/Userstate.jsx
+++ src/context/Userstate.jsx
@@ -1,18 +1,18 @@
-import React, { createContext, useContext, useState } from 'react'
+// import React, { createContext, useContext, useState } from 'react'
 
-const UserData = createContext();
+// const UserData = createContext();
 
-export const UserState = ({ children }) => {
+// export const UserState = ({ children }) => {
 
-    const [id, setId] = useState('1');
+//     const [id, setId] = useState('1');
 
-    return (
-        <UserData.Provider value={{ id, setId }}>
-            {children}
-        </UserData.Provider>
-    )
-}
+//     return (
+//         <UserData.Provider value={{ id, setId }}>
+//             {children}
+//         </UserData.Provider>
+//     )
+// }
 
-export const useMyContext = () => {
-    return useContext(UserData);
-};
+// export const useMyContext = () => {
+//     return useContext(UserData);
+// };
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { UserState } from './context/Userstate.jsx'
+// import { UserState } from './context/Userstate.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
