d02be1245a80fa25c48434f86d00fd437775ad17
vyomPriyesh <vyompriyesh@gmail.com>
2025-04-23 17:34:15 +0530

    addd

--- vite.config.js
+++ vite.config.js
@@ -8,18 +8,18 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5175,       
   },
-  build: {
-    chunkSizeWarningLimit: 10000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'react-vendor';
-            if (id.includes('lodash')) return 'lodash-vendor';
-            return 'vendor'; // All other libraries go here
-          }
-        }
-      }
-    }
-  }
+  // build: {
+  //   chunkSizeWarningLimit: 10000,
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes('node_modules')) {
+  //           if (id.includes('react')) return 'react-vendor';
+  //           if (id.includes('lodash')) return 'lodash-vendor';
+  //           return 'vendor'; // All other libraries go here
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 });
